2a6b05c3003fb469578952c571158e3f220f8274
Arindam saikia <arindamsaikia50a@gmail.com>
2024-09-25 14:36:51 +0530

    change the useState

--- src/components/Context API/NewsContext.jsx
+++ src/components/Context API/NewsContext.jsx
@@ -7,16 +7,17 @@ export const NewsContext = createContext();
 export const NewsProvider = ({ children }) => {
 
     const API_KEY = "bdf1f79659894daf9d8c927d1ee97728";
-    const [search ,setSearch] = useState("india");
+    const [search ,setSearch] = useState("America");
     const [newsData,setNewsData] = useState([]);
 
-        const fetchData = async() =>{
-          const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
-          const jsonData = await response.json();
-          console.log(jsonData.articles);
-          setNewsData(jsonData.articles);
-        }
-
+ 
+    const fetchData = async () =>{
+      const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+      const jsonData = await response.json();
+      console.log(jsonData.articles);
+      setNewsData(jsonData.articles);
+    }
+ 
         useEffect(()=> {
             fetchData()
         },[])  
